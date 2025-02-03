338e203234d92a07e99bc1c2deaec9a5d370531f
Guilherme Samuel <guisamuel800@gmail.com>
2025-02-03 14:34:28 -0300

    feat: setup jump and fall animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,23 @@
 extends CharacterBody2D
 
 const SPEED : int = 4200
-const JUMP_VELOCITY : int = -1000
+const JUMP_VELOCITY : int = -600
+var sprite : AnimatedSprite2D
+
+func _ready() -> void:
+	sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = JUMP_VELOCITY
+		sprite.play("run")
+	else:
+		if velocity.y < 0:
+			sprite.play("jump")
+		else:
+			sprite.play("fall")
 	move_and_slide()
