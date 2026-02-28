c60d75f23cbedf17e8a0c72d9464cdbdc6c43fd5
Guilherme Samuel <guisamuel800@gmail.com>
2026-02-27 22:54:53 -0300

    Add dynamic player jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,18 +2,22 @@ class_name Player extends CharacterBody2D
 
 const JUMP_VELOCITY : int = -600
 @onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
+var jumping : bool = false
 
-func _physics_process(delta: float) -> void:
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
+func _physics_process(delta: float) -> void:	
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_accept"):
 			velocity.y = JUMP_VELOCITY
+		
 		sprite.play("run")
 	else:
 		if velocity.y < 0:
 			sprite.play("jump")
+			if Input.is_action_just_released("ui_accept"):
+				velocity.y = 0
 		else:
 			sprite.play("fall")
+		
+		velocity += get_gravity() * delta
+
 	move_and_slide()
